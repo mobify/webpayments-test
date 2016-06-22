3269d7f1302e1d5d3d998a51066f785435b9aa58
Daniel Brook-Roberge <dbrookroberge@mobify.com>
2016-06-22 16:51:00 -0700

    Actually build on Heroku.

---
+++ webpack.prod.js
@@ -0,0 +1,20 @@
+var path = require('path');
+var webpack = require('webpack');
+
+module.exports = {
+  entry: [
+    './src/index'
+  ],
+  output: {
+    path: path.join(__dirname, 'dist'),
+    filename: 'bundle.js',
+    publicPath: '/static/'
+  },
+  module: {
+    loaders: [{
+      test: /\.js$/,
+      loaders: ['babel'],
+      include: path.join(__dirname, 'src')
+    }]
+  }
+};
