80f6d0a37c4f0373b7ce22703c5439159342b104
Daniel Brook-Roberge <dbrookroberge@mobify.com>
2016-06-22 17:04:04 -0700

    NVM.

--- express.js
+++ express.js
@@ -7,11 +7,11 @@ let app = express();
 app.set('port', (process.env.PORT || 3000));
 
 app.get('/', (req, res) => {
-    res.sendFile('/app/src/index.html')
+    res.sendFile('src/index.html', {root: __dirname})
 });
 
 app.get('/static/bundle.js', (req, res) => {
-    res.sendFile('/app/dist/bundle.js')
+    res.sendFile('dist/bundle.js', {root: __dirname})
 });
 
 app.listen(app.get('port'), () => console.log('App is running on port', app.get('port')));
