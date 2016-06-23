2cc1b1adfc26673fc5d0188d69535d333afc0a9c
Daniel Brook-Roberge <dbrookroberge@mobify.com>
2016-06-22 17:02:11 -0700

    Now I understand how sendFile works in express.

--- express.js
+++ express.js
@@ -7,11 +7,11 @@ let app = express();
 app.set('port', (process.env.PORT || 3000));
 
 app.get('/', (req, res) => {
-    res.sendFile('src/index.html')
+    res.sendFile('/app/src/index.html')
 });
 
 app.get('/static/bundle.js', (req, res) => {
-    res.sendFile('dist/bundle.js')
+    res.sendFile('/app/dist/bundle.js')
 });
 
 app.listen(app.get('port'), () => console.log('App is running on port', app.get('port')));
